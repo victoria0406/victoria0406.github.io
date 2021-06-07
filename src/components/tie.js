639ce2bc0c1107b6fc1af47eed440279edc66071
Whit3Snow <76245628+Whit3Snow@users.noreply.github.com>
2021-06-07 13:29:34 +0900

    lose and tie

---
+++ src/components/tie.js
@@ -0,0 +1,22 @@
+  
+import React from 'react';
+import imgfile from '../tie.png';
+import '../style/win.css';
+import { Link } from 'react-router-dom';
+import Menubar from './menu2';
+
+function tie(props){
+    return(
+    <div className="App-header">
+        <header className="header">
+        <div className="team-name"> {props.location.state.group} </div>
+        <div className="back"></div>
+        <img className="con3" src={imgfile} />
+        <Link to={{pathname :'./management', state : {group: props.location.state.group, user:props.location.state.user}}}><button className="OK">OK!</button> </Link>
+        <Menubar group={props.location.state.group} user={props.location.state.user}/>
+        </header>     
+    </div>
+    );
+}
+
+export default tie;
