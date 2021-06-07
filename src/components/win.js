df363f56093402752efec8f25286c3ec4e992815
kmin638123 <84027219+kmin638123@users.noreply.github.com>
2021-06-07 12:49:20 +0900

    Add files via upload

--- src/components/win.js
+++ src/components/win.js
@@ -8,7 +8,7 @@ function win(props){
     return(
     <div className="App-header">
         <header className="header">
-        <div className="team-name"> HELLO BADMINTON </div>
+        <div className="team-name"> {props.location.state.group} </div>
         <div className="back"></div>
         <img className="con" src={imgfile} />
         <Link to={{pathname :'./management', state : {group: props.location.state.group, user:props.location.state.user}}}><button className="Get">GET MILEAGE!</button> </Link>
